67b054d7a44cea14e01a272f49ff12caafdaaa18
Roel Gonzalez <roelgonzalez94@gmail.com>
2019-02-12 18:14:07 +0100

    add favicon

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -7,6 +7,11 @@
 <!-- Bootstrap -->
 <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+<link rel="manifest" href="images/favicon/site.webmanifest">
+
 @yield('stylesheets')
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->

483bd7f5ddcc5a09b5655d6d26cd5f3f7cf54012
Roel Gonzalez <roelgonzalez94@gmail.com>
2019-03-18 02:49:58 +0100

    add cookie notification

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -32,3 +32,23 @@
         gtag('config', 'UA-134452334-2');
     </script>
 @endif
+
+
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+<script>
+    window.addEventListener("load", function(){
+        window.cookieconsent.initialise({
+            "palette": {
+                "popup": {
+                    "background": "#edeff5",
+                    "text": "#838391"
+                },
+                "button": {
+                    "background": "#4b81e8"
+                }
+            },
+            "theme": "classic",
+            "position": "bottom-right"
+        })});
+</script>

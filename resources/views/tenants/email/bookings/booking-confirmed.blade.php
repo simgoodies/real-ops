5dbfe8d9abf1a21812d291cae70e5a96836d531c
Roel Gonzalez <roelgonzalez94@gmail.com>
2019-04-02 01:59:14 +0200

    remove event description from e-mail

--- resources/views/tenants/email/bookings/booking-confirmed.blade.php
+++ resources/views/tenants/email/bookings/booking-confirmed.blade.php
@@ -6,7 +6,6 @@
 
 Event date: {{ date('Y-m-d', strtotime($event->start_date)) }} at {{ date('Hi', strtotime($event->start_time)) }}Z until {{ date('Hi', strtotime($event->end_time)) }}Z
 
-{!! nl2br(e($event->description)) !!}
 <hr>
 **Summary of booking for {{ $flight->callsign }}**
 

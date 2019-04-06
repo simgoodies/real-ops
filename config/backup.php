4b7bff10bfd2a831c62955bc4e8fc1290e98445f
Roel Gonzalez <roelgonzalez94@gmail.com>
2019-04-06 23:15:01 +0200

    change backup notifications to notify only for failures

--- config/backup.php
+++ config/backup.php
@@ -109,9 +109,6 @@ return [
             \Spatie\Backup\Notifications\Notifications\BackupHasFailed::class         => ['mail'],
             \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFound::class => ['mail'],
             \Spatie\Backup\Notifications\Notifications\CleanupHasFailed::class        => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessful::class     => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFound::class   => ['mail'],
-            \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessful::class    => ['mail'],
         ],
 
         /*

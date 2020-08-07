242f6c3fc154237a387b7f0497708d0f06b4fbb3
Roël Gonzalez <roelgonzalez94@gmail.com>
2020-08-07 16:27:35 +0200

    add event create button

--- resources/views/office-events/index.blade.php
+++ resources/views/office-events/index.blade.php
@@ -8,11 +8,16 @@
     <div class="container mx-auto">
         @include('partials._page-heading', ['title' => 'Manage Events'])
         @include('partials._messages')
+
+        <div class="mt-4 px-4">
+            <a class="btn btn-blue block" href="{{ route('office-events.create') }}">Create event</a>
+        </div>
+
         @foreach($events as $event)
-            <div class="mt-4 px-4">
-                <div class="text-xl">{{ $event->title }}</div>
+            <div class="mt-4 mx-4 px-4 rounded border bg-white">
+                <div class="mt-2 text-xl text-center">{{ $event->title }}</div>
                 <div class="block py-4">
-                    <a class="btn btn-blue" href="{{ route('office-events.show', ['event' => $event]) }}">Manage...</a>
+                    <a class="btn btn-blue-secondary block" href="{{ route('office-events.show', ['event' => $event]) }}">Manage...</a>
                 </div>
             </div>
         @endforeach
--- resources/views/partials/_page-heading.blade.php
+++ resources/views/partials/_page-heading.blade.php
@@ -1,3 +1,3 @@
-<div id="page-heading" class="page-heading">
+<div id="page-heading" class="py-4 px-6 border-b-2 border-gray-100 bg-white lg:rounded-b-lg">
     <h1 class="text-xl">{{ $title }}</h1>
 </div>

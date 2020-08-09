f9a35fbf41fac4032b33b79ebd8a7276294bf776
Roël Gonzalez <roelgonzalez94@gmail.com>
2020-08-09 16:36:44 +0200

    properly added font

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -17,6 +17,7 @@
 <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
 <link rel="manifest" href="images/favicon/site.webmanifest">
 
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400;1,600;1,700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.4.1/dist/alpine.min.js" defer></script>
 @livewireStyles
 

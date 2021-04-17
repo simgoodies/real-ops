ec96269d646446ddbd7ea3dfa47fe009e5a51b0f
Roël Gonzalez <roelgonzalez94@gmail.com>
2021-04-17 14:37:57 +0200

    fix where alert message showed boolean

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -1,15 +1,15 @@
 @if (session()->has('success') || request()->has('success'))
     <div class="p-2 text-green-800 bg-green-200 border-2 border-green-700">
-        {{ session()->get('success') || request()->get('success') }}
+        {{ session()->get('success') ?? request()->get('success') }}
     </div>
 @endif
 @if (session()->has('failure') || request()->has('failure'))
     <div class="p-2 text-red-800 bg-red-200 border-2 border-red-700">
-        {{ session()->get('failure') || request()->get('failure') }}
+        {{ session()->get('failure') ?? request()->get('failure') }}
     </div>
 @endif
 @if (session()->has('info') || request()->has('info'))
     <div class="p-2 text-blue-800 bg-blue-200 border-2 border-blue-700">
-        {{ session()->get('info') || request()->get('info') }}
+        {{ session()->get('info') ?? request()->get('info') }}
     </div>
 @endif
